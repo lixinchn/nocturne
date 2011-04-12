324178b354b11e66e3ec1ae3f23c96d25c546024
lixin <lixinit@gmail.com>
2011-04-12 14:36:26 +0800

    add any/some tail/rest, reject pluck, invoke to enumerable

--- nocturne.enumerable.js
+++ nocturne.enumerable.js
@@ -57,6 +57,12 @@ nocturne.enumerable = {
 		return results;
 	},
 
+	reject: function(enumerable, callback, context){
+		return this.filter(enumerable, function(){
+			return !callback.apply(context, arguments);
+		}, context);
+	},
+
 	reduce: function(enumerable, memo, callback, context){
 		if (Array.prototype.reduce && enumerable.reduce === Array.prototype.reduce){
 			return enumerable.reduce(nocturne.bind(callback, context), memo);
@@ -65,6 +71,38 @@ nocturne.enumerable = {
 			memo = callback.call(context, memo, value, index, list);
 		});
 		return memo;
+	},
+
+	tail: function(enumerable, start){
+		start = typeof start === 'undefined' ? 1: start;
+		return Array.prototype.slice.apply(enumerable, [start]);
+	},
+
+	invoke: function(enumerable, method){
+		var args = nocturne.enumerable.tail(arguments, 2);
+		return nocturne.enumerable.map(enumerable, function(value){
+			return (method ? value[method] : value).apply(value, args);
+		});
+	},
+
+	pluck: function(enumerable, key){
+		return nocturne.enumerable.map(enumerable, function(value){
+			return value[key];
+		});
+	},
+
+	some: function(enumerable, callback, context){
+		callback = callback || nocturne.enumerable.identity;
+		if (Array.prototype.some && enumerable.some === Array.prototype.some){
+			return enumerable.some(callback, context);
+		}
+		var result = false;
+		nocturne.enumerable.each(enumerable, function(value, index, list){
+			if (result = callback.call(context, value, index, list)){
+				throw nocturne.enumerable.Break;
+			}
+		});
+		return result;
 	},
 
 	detect: function(enumerable, callback, context){
@@ -80,12 +118,21 @@ nocturne.enumerable = {
 
 	chain: function(enumerable){
 		return new nocturne.enumerable.Chainer(enumerable);
+	},
+
+	identity: function(value){
+		return value;
 	}
 };
 
 //Alias
 nocturne.enumerable.select = nocturne.enumerable.filter;
 nocturne.enumerable.inject = nocturne.enumerable.reduce;
+nocturne.enumerable.collect = nocturne.enumerable.map;
+nocturne.enumerable.rest = nocturne.enumerable.tail;
+nocturne.enumerable.any = nocturne.enumerable.some;
+nocturne.chainableMethods = ['map', 'collect', 'detect', 'filter', 'reduce', 'tail', 'rest', 'reject', 'pluck', 'any', 'some'];
+
 
 //Chainer class
 nocturne.enumerable.Chainer = nocturne.Class({
@@ -98,7 +145,7 @@ nocturne.enumerable.Chainer = nocturne.Class({
 	}
 });
 
-nocturne.enumerable.each(['map', 'detect', 'filter', 'reduce'], function(methodName){
+nocturne.enumerable.each(nocturne.chainableMethods, function(methodName){
 	var method = nocturne.enumerable[methodName];
 	nocturne.enumerable.Chainer.prototype[methodName] = function(){
 		var args = Array.prototype.slice.call(arguments);
--- test/enumerable_test.js
+++ test/enumerable_test.js
@@ -27,6 +27,12 @@ Riot.context('nocturne.enumerable.js', function(){
 			});
 		}).equals([2, 4]);
 
+		should('reject arrays', function(){
+			return nocturne.enumerable.reject(a, function(n){
+				return n % 2 == 0
+			});
+		}).equals([1, 3, 5]);
+
 		should('find values with detect', function(){
 			return nocturne.enumerable.detect(a, function(n){
 				return n == 1;
@@ -52,6 +58,36 @@ Riot.context('nocturne.enumerable.js', function(){
 				return memo + n;
 			});
 		}).equals(6);
+
+		should('invoke methods on arrays', function(){
+			return nocturne.enumerable.invoke(['hello', 'world'], 'substring', 0, 3);
+		}).equals(['hel', 'wor']);
+
+		should('pluck properties from arrays', function(){
+			return nocturne.enumerable.pluck(['hello', 'world', 'this', 'is', 'nice'], 'length');
+		}).equals([5, 5, 4, 2, 4]);
+
+		should('tail by skipping the first element', function(){
+			return nocturne.enumerable.tail([1, 2, 3, 4, 5]);
+		}).equals([2, 3, 4, 5]);
+
+		should('tail by skipping an optional number of elements', function(){
+			return nocturne.enumerable.tail([1, 2, 3, 4, 5], 3);
+		}).equals([4, 5]);
+
+		should('return false from some when an array is empty', function(){
+			return nocturne.enumerable.some([]);
+		}).isFalse();
+
+		should('return true from some when an array has items', function(){
+			return nocturne.enumerable.some([1, 2, 3]);
+		}).isTrue();
+
+		should('return true from some when an array matches a callback', function(){
+			return nocturne.enumerable.some([1, 2, 3], function(value){
+				return value === 3;
+			});
+		}).isTrue();
 	});
 
 	given('an object', function(){
